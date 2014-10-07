25a7fe8f620df33b11784365c25a7ac194f5d532
Matthias Flaig <matt.flaig@web.de>
2014-10-07 13:26:07 +0200

    namespace for admin

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -61,21 +61,6 @@ RSpec.describe ProjectsController, :type => :controller do
         expect(assigns(:project)).to be_instance_of(Project)
       end
     end
-
-    # context "with standard users" do 
-    #   let(:user){FactoryGirl.create(:user, :admin => false)}
-    #   before do 
-    #     sign_in user
-    #     get :new
-    #   end
-
-    #   it "redirects to rootpath" do 
-    #     expect(response).to redirect_to(root_path)
-    #   end
-    #   it "sets an error message" do
-    #     expect(flash[:alert]).to eql("You must be an admin to do that!")
-    #   end
-    # end
 	end
 
 	describe "POST#create" do
