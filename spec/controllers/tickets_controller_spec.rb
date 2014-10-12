3e858017615e0869ed1a98657386fae61cf75c94
Matthias Flaig <matt.flaig@web.de>
2014-10-12 13:43:40 +0200

    update permission

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -31,10 +31,6 @@ RSpec.describe TicketsController, :type => :controller do
         expect(flash[:alert]).to eq("You cannot create tickets on this project.")
       end 
     end
-
-    context "with standard users who have permission" do 
-
-    end
   end
 
   describe "GET#show" do 
@@ -50,7 +46,7 @@ RSpec.describe TicketsController, :type => :controller do
       end
     end
 
-    context "with standard users who don't have permission" do
+    context "with standard users who don't have view permission" do
       let(:pro1){FactoryGirl.create(:project)}
       let(:tic1){FactoryGirl.create(:ticket, project_id: pro1.id)}
       let(:user){FactoryGirl.create(:user, admin: false)}
@@ -68,84 +64,106 @@ RSpec.describe TicketsController, :type => :controller do
   end
 
   describe "POST#create" do 
+    context "with admins" do
+      context "with valid input" do 
+      	let (:pro1) {FactoryGirl.create(:project)}
+        let(:admin){FactoryGirl.create(:user, admin: true)}
+      	before do 
+          sign_in admin 
+        	tic1 = FactoryGirl.attributes_for(:ticket)
+        	post :create, ticket: tic1, project_id: pro1.id, user_id: admin.id
+      	end
+        it "creates the ticket" do 
+        	expect(Ticket.count).to eq(1)
+        end
+        it "associates ticket with project" do 
+          expect(Ticket.first.project_id).to eq(pro1.id)
+        end
+        it "sets a success message" do
+        	expect(flash[:notice]).to be_present
+        end
+        it "redirects to ticket page" do 
+          expect(response).to redirect_to '/projects/1/tickets/1'
+        end
+      end
+      context "with invalid input" do
+        let(:admin){FactoryGirl.create(:user, admin: true)}
 
-    context "with valid input" do 
-    	let (:pro1) {FactoryGirl.create(:project)}
-      let(:admin){FactoryGirl.create(:user, admin: true)}
-    	before do 
-        sign_in admin 
-      	tic1 = FactoryGirl.attributes_for(:ticket)
-      	post :create, ticket: tic1, project_id: pro1.id, user_id: admin.id
-    	end
-      it "creates the ticket" do 
-      	expect(Ticket.count).to eq(1)
-      end
-      it "associates ticket with project" do 
-        expect(Ticket.first.project_id).to eq(pro1.id)
-      end
-      it "sets a success message" do
-      	expect(flash[:notice]).to be_present
-      end
-      it "redirects to ticket page" do 
-        expect(response).to redirect_to '/projects/1/tickets/1'
-      end
-    end
-    context "with invalid input" do
-      let(:admin){FactoryGirl.create(:user, admin: true)}
-
-    	before do 
-        sign_in admin
-    		pro1 = FactoryGirl.create(:project)
-      	post :create, ticket: {title: "invalid"}, project_id: pro1.id, user_id: admin.id
-      end
-      it "does not create a ticket" do 
-        expect(Ticket.count).to eq(0)
-      end
-      it "sets an error message" do 
-        expect(flash[:alert]).to be_present
-      end
-      it "renders the new template" do 
-        expect(response).to render_template :new
+      	before do 
+          sign_in admin
+      		pro1 = FactoryGirl.create(:project)
+        	post :create, ticket: {title: "invalid"}, project_id: pro1.id, user_id: admin.id
+        end
+        it "does not create a ticket" do 
+          expect(Ticket.count).to eq(0)
+        end
+        it "sets an error message" do 
+          expect(flash[:alert]).to be_present
+        end
+        it "renders the new template" do 
+          expect(response).to render_template :new
+        end
       end
     end
   end
 
-  describe "PUT#update" do 
-    context "with valid input" do 
-      let(:admin){ FactoryGirl.create(:user, admin: true)}
-      
-    	before do 
-        sign_in admin
-    		pro1 = FactoryGirl.create(:project)
-        tic1 = FactoryGirl.create(:ticket, project_id: pro1.id, user_id: admin.id)
-        put :update, {id: tic1, ticket:{title: "another one"}, project_id: tic1.project_id}
-      end
-      it "updates the record" do 
-        expect(Ticket.first.title).to eq("another one")
-      end
-      it "sets a flash success message" do 
-      	expect(flash[:notice]).to be_present
-      end
-      it "redirects to ticket page" do 
-      	expect(response).to redirect_to '/projects/1/tickets/1'
-      end
-    end 
-    context "with invalid input" do 
-    	let (:pro1) {FactoryGirl.create(:project)}
-      let (:admin) {FactoryGirl.create(:user, admin: true)}
-    	let (:tic1) {FactoryGirl.create(:ticket, project_id: pro1.id, user_id: admin.id)}
+  describe "PUT#update" do
+    context "with standard users who do not have permission" do 
+      let(:pro1){FactoryGirl.create(:project)}
+      let(:tic1){FactoryGirl.create(:ticket, project_id: pro1.id)}
+      let(:user){FactoryGirl.create(:user, admin: false)}
       before do 
-        sign_in admin
-        put :update, {id: tic1, ticket:{title: ""}, project_id: tic1.project_id}
+        sign_in user
+        permission = Permission.create(user: user, thing: pro1, action: "view") 
+        put :update, {project_id: pro1.id, id: tic1.id, ticket:{}}
       end
-      it "does not update the record" do 
-        expect(Ticket.first).to eq(tic1)
+
+      it "redirects to project" do 
+        expect(response).to redirect_to(:project)
       end
+
       it "sets an error message" do 
-      	expect(flash[:alert]).to be_present
+        expect(flash[:alert]).to eq("You cannot edit tickets on this project.")
       end
-      it "renders the edit page" do 
-      	expect(response).to render_template :edit
+    end
+
+    context "with admins" do  
+      context "with valid input" do 
+        let(:admin){ FactoryGirl.create(:user, admin: true)}
+        
+      	before do 
+          sign_in admin
+      		pro1 = FactoryGirl.create(:project)
+          tic1 = FactoryGirl.create(:ticket, project_id: pro1.id, user_id: admin.id)
+          put :update, {id: tic1, ticket:{title: "another one"}, project_id: tic1.project_id}
+        end
+        it "updates the record" do 
+          expect(Ticket.first.title).to eq("another one")
+        end
+        it "sets a flash success message" do 
+        	expect(flash[:notice]).to be_present
+        end
+        it "redirects to ticket page" do 
+        	expect(response).to redirect_to '/projects/1/tickets/1'
+        end
+      end 
+      context "with invalid input" do 
+      	let (:pro1) {FactoryGirl.create(:project)}
+        let (:admin) {FactoryGirl.create(:user, admin: true)}
+      	let (:tic1) {FactoryGirl.create(:ticket, project_id: pro1.id, user_id: admin.id)}
+        before do 
+          sign_in admin
+          put :update, {id: tic1, ticket:{title: ""}, project_id: tic1.project_id}
+        end
+        it "does not update the record" do 
+          expect(Ticket.first).to eq(tic1)
+        end
+        it "sets an error message" do 
+        	expect(flash[:alert]).to be_present
+        end
+        it "renders the edit page" do 
+        	expect(response).to render_template :edit
+        end
       end
     end
   end
